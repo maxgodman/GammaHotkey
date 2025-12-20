17ceeaa52eca8a2a69ae2989e95d91ff399876b9
maxgodman <11789832+maxgodman@users.noreply.github.com>
2025-12-20 00:17:59 +0000

    Add resource files

---
+++ resources/Resource.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2025 Max Godman
+
+//{{NO_DEPENDENCIES}}
+
+#define IDS_APP_TITLE			103
+#define IDI_GAMMAHOTKEY			104
+#define IDI_SMALL				105
+#define IDC_GAMMAHOTKEY			106
+#define IDI_ON					107
+#define IDI_OFF					108
+
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+
+// Next default values for new objects.
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
