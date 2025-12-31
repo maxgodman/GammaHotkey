48737b80e0304cdbb7824f9366a9840d8e46c8cb
maxgodman <11789832+maxgodman@users.noreply.github.com>
2025-12-31 01:35:13 +0000

    Resources updated, various app info and versioning is now defined

--- resources/Resource.h
+++ resources/Resource.h
@@ -11,7 +11,20 @@
 
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
-#endif
+#endif // IDC_STATIC
+
+#define VER_FILEVERSION			0,1
+#define VER_FILEVERSION_STR		"0.1"
+#define VER_PRODUCTVERSION		0,1
+#define VER_PRODUCTVERSION_STR	"0.1"
+
+#define VER_COMPANYNAME			"Max Godman"
+#define VER_FILEDESCRIPTION		"Display gamma adjustment using hotkeys"
+#define VER_INTERNALNAME		"GammaHotkey"
+#define VER_LEGALCOPYRIGHT		"Copyright (c) 2025 Max Godman"
+#define VER_ORIGINALFILENAME	"GammaHotkey.exe"
+#define VER_PRODUCTNAME			"GammaHotkey"
+#define VER_PRODUCTNAME_W		L"GammaHotkey"
 
 // Next default values for new objects.
 //
@@ -23,5 +36,5 @@
 #define _APS_NEXT_COMMAND_VALUE		32771
 #define _APS_NEXT_CONTROL_VALUE		1000
 #define _APS_NEXT_SYMED_VALUE		110
-#endif
-#endif
+#endif // APSTUDIO_READONLY_SYMBOLS
+#endif // APSTUDIO_INVOKED
